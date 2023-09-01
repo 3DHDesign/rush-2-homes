c1a48d0753141a09ac7abe79705646a74fc9a311
Tharuka Idushan <tharukaidushan@gmail.com>
2023-09-01 10:21:39 +0530

    update header lang file

---
+++ lang/si/homepage.php
@@ -0,0 +1,14 @@
+<?php
+
+// Sinhala language php
+
+return [
+    'header' => [
+        'home' => 'මුල පිටුව',
+        'sell' => 'විකුණන්න',
+        'rent' => 'කුලියට',
+        'land' => 'ඉඩම්',
+        'contact_us' => 'අප අමතන්න',
+        'add_new_property' => 'නව දේපලක්',
+    ]
+];
