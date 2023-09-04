d06e211b3c12262c3ef10121d77e257a5beba2af
Tharuka Idushan <tharukaidushan@gmail.com>
2023-09-04 22:23:52 +0530

    fi language errors

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetLocale
@@ -13,8 +14,15 @@ class SetLocale
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
+        $lang = 'en'; // defualt language
+
+        app()->setLocale($request->segment(1) ?? $lang);
+        session()->put('locale', $request->segment(1) ?? $lang);
+
+        URL::defaults(['locale' => $request->segment(1)]);
+
         return $next($request);
     }
 }
