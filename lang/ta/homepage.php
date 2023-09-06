021e06cc076fedd7dc0f554efc250f3d3eb55888
Tharuka Idushan <tharukaidushan@gmail.com>
2023-09-07 00:15:58 +0530

    add tamil homepage file

---
+++ lang/ta/homepage.php
@@ -0,0 +1,14 @@
+<?php
+
+// Tamil language php
+
+return [
+    'header' => [
+        'home' => 'வீடு',
+        'sell' => 'விற்க',
+        'rent' => 'வாடகை',
+        'land' => 'நில',
+        'contact_us' => 'எங்களை தொடர்பு கொள்ள',
+        'add_new_property' => 'புதிய சொத்து சேர்க்க',
+    ]
+];
