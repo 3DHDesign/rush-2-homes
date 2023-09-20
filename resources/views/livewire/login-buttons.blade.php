027d4984ee59917e914812c518e27e92420ba954
Tharuka Idushan <tharukaidushan@gmail.com>
2023-09-20 15:29:07 +0530

    add dashboard link to header

--- resources/views/livewire/login-buttons.blade.php
+++ resources/views/livewire/login-buttons.blade.php
@@ -1,8 +1,15 @@
 <div>
     @if (auth()->guard('client')->user() || auth()->user())
-        <li style="margin-left: 20px;">
-            <a wire:click="logout" class="btn btn-primary"><i class="feather-user-minus"></i>Logout</a>
-        </li>
+        <ul class="nav header-navbar-rht">
+            <li style="margin-left: 20px;">
+                <a wire:click="logout" class="btn btn-primary"><i class="feather-user-minus"></i>Logout</a>
+            </li>
+            <li class="new-property-btn">
+                <a href="{{ route('user.dashboard.home') }}">
+                    Go to Dashboard
+                </a>
+            </li>
+        </ul>
     @else
         <ul class="nav header-navbar-rht">
 
